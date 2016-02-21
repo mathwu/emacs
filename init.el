e5bf187994820161bbb8d90baf42dab5f1ae1c5c
mathwu <sewu@outlook.com>
2016-02-21 09:10:51 +0800

    clean

---
+++ init.el
@@ -0,0 +1,188 @@
+;; coding information
+(prefer-coding-system 'utf-8)
+
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
+
+;;----------------------------------------------------------------------------
+;; Bootstrap config
+;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(require 'init-compat)
+(require 'init-utils)
+(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+;; Calls (package-initialize)
+(require 'init-elpa)      ;; Machinery for installing required packages
+(require 'init-exec-path) ;; Set up $PATH
+
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-preload-local.el"
+;;----------------------------------------------------------------------------
+(require 'init-preload-local nil t)
+
+;;----------------------------------------------------------------------------
+;; Load configs for specific features and modes
+;;----------------------------------------------------------------------------
+
+(require-package 'wgrep)
+(require-package 'project-local-variables)
+(require-package 'diminish)
+(require-package 'scratch)
+(require-package 'mwe-log-commands)
+
+(require 'init-frame-hooks)
+(require 'init-xterm)
+(require 'init-themes)
+(require 'init-osx-keys)
+(require 'init-gui-frames)
+(require 'init-dired)
+(require 'init-isearch)
+(require 'init-grep)
+(require 'init-uniquify)
+(require 'init-ibuffer)
+(require 'init-flycheck)
+
+(require 'init-recentf)
+(require 'init-ido)
+(require 'init-hippie-expand)
+(require 'init-auto-complete)
+(require 'init-windows)
+(require 'init-sessions)
+(require 'init-fonts)
+(require 'init-mmm)
+
+(require 'init-editing-utils)
+(require 'init-whitespace)
+(require 'init-fci)
+
+(require 'init-vc)
+(require 'init-darcs)
+(require 'init-git)
+(require 'init-github)
+
+(require 'init-compile)
+(require 'init-crontab)
+(require 'init-textile)
+(require 'init-markdown)
+(require 'init-csv)
+(require 'init-erlang)
+(require 'init-javascript)
+(require 'init-php)
+(require 'init-org)
+(require 'init-nxml)
+(require 'init-html)
+(require 'init-css)
+(require 'init-haml)
+(require 'init-python-mode)
+(require 'init-haskell)
+(require 'init-elm)
+(require 'init-ruby-mode)
+(require 'init-rails)
+(require 'init-sql)
+
+(require 'init-paredit)
+(require 'init-lisp)
+(require 'init-slime)
+(unless (version<= emacs-version "24.2")
+  (require 'init-clojure)
+  (require 'init-clojure-cider))
+(require 'init-common-lisp)
+
+(when *spell-check-support-enabled*
+  (require 'init-spelling))
+
+(require 'init-misc)
+
+(require 'init-dash)
+(require 'init-ledger)
+;; Extra packages which don't require any configuration
+
+(require-package 'gnuplot)
+(require-package 'lua-mode)
+(require-package 'htmlize)
+(require-package 'dsvn)
+(when *is-a-mac*
+  (require-package 'osx-location))
+(require-package 'regex-tool)
+
+;;----------------------------------------------------------------------------
+;; Allow access from emacsclient
+;;----------------------------------------------------------------------------
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+
+;;----------------------------------------------------------------------------
+;; Variables configured via the interactive 'customize' interface
+;;----------------------------------------------------------------------------
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-local" containing personal settings
+;;----------------------------------------------------------------------------
+(when (file-exists-p (expand-file-name "init-local.el" user-emacs-directory))
+  (error "Please move init-local.el to ~/.emacs.d/lisp"))
+(require 'init-local nil t)
+
+
+;;----------------------------------------------------------------------------
+;; Locales (setting them earlier in this file doesn't work in X)
+;;----------------------------------------------------------------------------
+(require 'init-locales)
+
+(provide 'init)
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; set directories
+(when (eq system-type 'windows-nt)
+  (defvar base-path "D:/emacs"))
+(when (eq system-type 'darwin)
+  (defvar base-path "~/.emacs.d"))
+
+(defvar plugin-path (concat base-path "/plugins"))
+(defvar config-path (concat base-path "/my-config"))
+(defvar temp-path (concat base-path "/temp"))
+(defvar plugin-config-path (concat config-path "/plugin-config"))
+(defvar elisp-fun-path (concat config-path "/elisp-fun"))
+
+(require 'package)
+(require 'init-benchmarking) ;; Measure startup time
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			   ("marmalage" . "http://marmalade-repo.org/packages/")
+			   ("org" . "http://orgmode.org/elpa/")
+			   ("melpa" . "http://melpa.milkbox.net/packages/")
+			   ("popkit" . "http://elpa.popkit.org/packages/")))
+(package-initialize)
+
+;; define package list
+(setq my-package-list
+      '(color-theme
+	org
+	auctex
+	reftex
+	auto-complete ;; an popular auto complete system
+	yasnippet ;; yet another snippets
+	git ;; git support
+	))
+; start emacs in server mode so that skim can talk to it
+(server-start)
