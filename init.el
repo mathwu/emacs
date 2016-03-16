0217d32fc5a319f6108006aa644f822dedaad1d0
mathwu <sewu@outlook.com>
2016-03-16 10:33:58 +0800

    微调

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
  (when *is-a-linux*
  (custom-set-variables
     '(cfs--current-profile-name "linux" t)
-    '(cfs--fontsize-steps (quote (7 4 3)) t)))
+    '(cfs--fontsize-steps (quote (8 4 4)) t)))
 
 
 
@@ -296,7 +296,6 @@
 (require 'server)
 (unless (server-running-p)
   (server-start))
-
 
 
 ;; Local Variables:
